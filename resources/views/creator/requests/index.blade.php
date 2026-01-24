6a78849ebe57f0904df5432f0fbdcd53ce3fc4e7
SolaTukishiro <antaku0514@icloud.com>
2026-01-24 22:01:17 +0900

    creator requestの公開変更タブ追加

--- resources/views/creator/requests/index.blade.php
+++ resources/views/creator/requests/index.blade.php
@@ -3,6 +3,20 @@
         <h2 class="application-index__page-title">公開依頼一覧</h2>
     </x-slot>
     <section class="application-index">
+        <nav class="request-tabs" aria-label="依頼フィルター">
+            <a href="{{ route('creator.requests.show', ['tab' => 'open']) }}"
+               class="request-tabs__item {{ $tab === 'open' ? 'is-active' : '' }}">
+                受付中
+            </a>
+            <a href="{{ route('creator.requests.show', ['tab' => 'closed']) }}"
+               class="request-tabs__item {{ $tab === 'closed' ? 'is-active' : '' }}">
+                受付停止中
+            </a>
+            <a href="{{ route('creator.requests.show', ['tab' => 'all']) }}"
+               class="request-tabs__item {{ $tab === 'all' ? 'is-active' : '' }}">
+                すべて
+            </a>
+        </nav>
         <div class="request-index__container">
             <div class="request-index__grid">
                 @forelse($requests as $request)
